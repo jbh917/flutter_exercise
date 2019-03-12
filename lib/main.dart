c8bce6eeb49061ef9d2788ffe0b912856f6b0f48
jbh917 <jbh915@gmail.com>
2019-03-12 13:43:42 +0900

    Creating listview and understanding layers

--- lib/main.dart
+++ lib/main.dart
@@ -3,41 +3,62 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
-//stateless는 한번 만들어지고 불변이므로 hot reload 하더라도 text값이 변경되지 않는다.
-//stateful 로 작성하면 hot reload하면 text 값이 변경 된다.
-//stateful은 statefulWidget클래스와 state 클래스가 필요하다.
-
-
 class MyApp extends StatelessWidget {
-//  final wordPair = new WordPair.random();                       stateless
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: new Center(
-//          child: const Text('Hello World'),                     stateless
-        child: new RandomWords(),
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: new RandomWords(),
     );
   }
 }
+// MaterialApp은 title과 home 으로 이루어져 있다.
 
-class RandomWordsState extends State<RandomWords>{
+class RandomWordsState extends State<RandomWords> {
+
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final WordPair wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator')
+      ),
+      body: _buildSuggestions()
+    );
   }
 
+  //Scaffold는 Material Design의 기본 골격을 쉽게 추가 할 수 있게함.
+  //AppBar와 Body를 추가하는 방식
+  //body는 하나의 위젯만을 가진다. 여러가지 위젯을 포함시키기위해 위젯을 body의 child로 설정하는것이 필요
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return new Divider();
+          }
+          final int index = i ~/ 2;
+
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+        title: new Text(
+      pair.asPascalCase,
+      style: _biggerFont,
+    ));
+  }
 }
 
-class RandomWords extends StatefulWidget{
+class RandomWords extends StatefulWidget {
   @override
   RandomWordsState createState() => new RandomWordsState();
-
 }
